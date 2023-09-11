f4327777b208c626f67f800275caa5ef3f69675d
Akash Khanra <akhanra@amazon.com>
2023-09-12 03:51:52 +0530

    Finalised All Modules Codes

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import theme from './theme';
+import {Provider} from "react-redux";
 import reportWebVitals from './reportWebVitals';
+import {ChakraProvider} from '@chakra-ui/react'
+import {RouterProvider} from "react-router-dom";
+import router from "./routes";
+import {store} from "./store";
+import {QueryClientProvider} from "@tanstack/react-query";
+import {queryClient} from "./services/api-client";
+import '@fontsource/raleway/400.css';
+import '@fontsource/open-sans/700.css';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <ChakraProvider theme={theme}>
+            <Provider store={store}>
+                <QueryClientProvider client={queryClient}>
+                    <RouterProvider router={router}/>
+                </QueryClientProvider>
+            </Provider>
+        </ChakraProvider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
